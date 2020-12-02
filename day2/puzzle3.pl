92608386d810efd7bcbbc3276188350d31e23171
Ron Geens <ronald.geens@gmail.com>
2020-12-02 09:34:00 +0100

    solved puzzle3

---
+++ day2/puzzle3.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my $debug=1;
+
+my $validcount = 0;
+
+while(<>) {
+    if (/(\d+)-(\d+)\s+(.):\s+(.*)/) {
+        my $min = $1;
+        my $max = $2;
+        my $char = $3;
+        my $pass = $4;
+
+        print("min=$min, max=$max, char=$char, pass=$pass  ") if $debug;
+    
+        my $charcount = () = $pass =~ m/$char/g;
+        print("count=$charcount => ") if $debug;
+
+        if ($min <= $charcount && $charcount <= $max) {
+            print("valid\n") if $debug;
+            $validcount++;
+        } else {
+            print("FAIL!\n") if $debug;
+        }
+    }
+}
+
+print("Found $validcount valid passwords\n")
